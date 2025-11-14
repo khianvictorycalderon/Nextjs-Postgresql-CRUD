c1afe9a466df699a8dbdd04780aef6ed3607105b
khian calderon <ckhianvictory@gmail.com>
2025-11-14 21:11:47 +0800

    Added error handling client side

---
+++ lib/error-handling-client.ts
@@ -0,0 +1,6 @@
+export default function handleClientError(err: unknown, contextMessage = "An error occurred") {
+  const message = err instanceof Error ? err.message : String(err);
+
+  // Alert the user
+  alert(`${contextMessage}: ${message}`);
+}
