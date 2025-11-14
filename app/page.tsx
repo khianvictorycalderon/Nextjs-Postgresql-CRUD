23eacae199a8a6bce006d7ec8261abcf8c83f0b1
khian calderon <ckhianvictory@gmail.com>
2025-11-14 21:51:07 +0800

    Successfully created fetch all users

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import handleClientError from "@/lib/error-handling-client";
 import SectionContainer from "@/lib/section-container";
+import { AllUsersProps } from "@/lib/types";
 import { HeadingText, HeroHeadingText } from "@/lib/typography";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -10,14 +11,18 @@ const userNameInputField = "user_name";
 
 export default function App() {
 
-  // Requests to the API to automatically create a table if not yet exists
-  useEffect(() => {
-    async function initialTableCreation() {
-      await axios.post("/api/db");
+  // For fetching existing users
+  const [allUsers, setAllUsers] = useState<AllUsersProps[]>([]);
+  const handleFetchAllUsers = async () => {
+    try {
+      const res = await axios.get("/api/user");
+      setAllUsers(res.data.users);
+    } catch (err: unknown) {
+      handleClientError(err, "Failed to fetch all users");
     }
-    initialTableCreation();
-  },[]);
+  }
 
+  // For creating users
   const [isSubmittingUserName, setIsSubmittingUserName] = useState<boolean>(false);
   const handleCreateUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Stops the browser from refreshing
@@ -39,10 +44,20 @@ export default function App() {
     } catch (err: unknown) {
       handleClientError(err, "Failed to create user");
     } finally {
+      await handleFetchAllUsers();
       setIsSubmittingUserName(false);
       form.reset(); // Clears the input field
     }
   }
+
+  // Requests to the API to automatically create a table if not yet exists
+  useEffect(() => {
+    async function initialTableCreation() {
+      await axios.post("/api/db");
+    }
+    initialTableCreation();
+    handleFetchAllUsers();
+  },[]);
 
   return (
     <>
@@ -54,6 +69,23 @@ export default function App() {
           <input disabled={isSubmittingUserName} id={userNameInputField} name={userNameInputField} className="disabled:bg-gray-300 disabled:text-gray-600 disabled:cursor-not-allowed bg-gray-100 p-1 text-black rounded-md outline-none ring-blue-600 focus:ring-2" type="text" />
           <button disabled={isSubmittingUserName} className="px-4 py-2 disabled:bg-gray-400 disabled:text-gray-300 disabled:cursor-not-allowed bg-blue-600 text-white rounded-md cursor-pointer hover:bg-blue-500 transition duration-300" type="submit">Add New User</button>
         </form>
+
+        <table className="mt-4 border-collapse [&_th]:border-1 [&_th]:p-2 [&_td]:text-center [&_td]:border-1 [&_td]:p-2 w-full">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User Names</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUsers.map((item, index) => (
+              <tr key={`${item.user_name}-${index}`}>
+                <td>{item.user_id}</td>
+                <td>{item.user_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
       </SectionContainer>
     </>
---
+++ lib/types.ts
@@ -0,0 +1,5 @@
+'use client'
+export interface AllUsersProps {
+    user_id: string;
+    user_name: string;
+}
