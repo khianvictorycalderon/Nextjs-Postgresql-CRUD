16fe6c6e8fe08693305d0eadcb445d65198d4b5d
khian calderon <ckhianvictory@gmail.com>
2025-11-14 20:50:59 +0800

    Added intial table creation query

--- app/api/db/route.ts
+++ app/api/db/route.ts
@@ -1,11 +1,25 @@
 import pool from "@/lib/db";
+import { dev } from "@/lib/dev-log";
+import { NextResponse } from "next/server";
 
 // This code automatically creates table for the user if it still does not exist
 export async function POST() {
-    const res = await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            user_id SERIAL PRIMARY KEY NOT NULL,
-            user_name VARCHAR(200)
-        )    ;
-    `);
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                user_id SERIAL PRIMARY KEY NOT NULL,
+                user_name VARCHAR(200)
+            )    ;
+        `);
+        dev.log("Table users successfully created!");
+        return NextResponse.json({ message: "Table users successfully created!" }, { status: 200 })
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            dev.log(`Failed to create users table: ${err.message}`);
+            return NextResponse.json({ message: `Failed to create users table: ${err.message}`}, { status: 500 });
+        } else {
+            dev.log(`Failed to create users table: ${String(err)}`);
+            return NextResponse.json({ message: `Failed to create users table: ${String(err)}`}, { status: 500 });
+        }
+    }
 }
