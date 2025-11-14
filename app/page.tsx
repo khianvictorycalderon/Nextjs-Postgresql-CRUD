e705c985096315c5c4b0e9ad587a38d4140b3c53
khian calderon <ckhianvictory@gmail.com>
2025-11-14 20:34:20 +0800

    Added handle create user

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+'use client';
 import SectionContainer from "@/lib/section-container";
-import { HeroHeadingText } from "@/lib/typography";
+import { HeadingText, HeroHeadingText } from "@/lib/typography";
 
 export default function App() {
+
+  const handleCreateUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Stops the browser from refreshing
+    
+    const formData = new FormData(e.currentTarget);
+    const userName = formData.get("user_name");
+
+    alert(`New user name is ${userName}`);
+    e.currentTarget.reset(); // Clears the input field
+
+  }
+
   return (
     <>
       <SectionContainer className="mt-8">
         <HeroHeadingText className="text-center">Next.js + Postgresql CRUD</HeroHeadingText>
+        
+        <form onSubmit={handleCreateUser} className="flex gap-2 mt-8">
+          <label><HeadingText>Create User (Username):</HeadingText></label>
+          <input name="user_name" className="bg-gray-100 p-1 text-black rounded-md outline-none ring-blue-600 focus:ring-2" type="text" />
+          <button className="px-4 py-2 bg-blue-600 text-white rounded-md cursor-pointer hover:bg-blue-500 transition duration-300" type="submit">Add New User</button>
+        </form>
+
       </SectionContainer>
     </>
   );
